d6ee509dcdc93efd2fd097e8a1cbb8eb80b2f415
ergazee <wangshuguang321@gmail.com>
2020-11-07 23:23:17 +0800

    V1.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_173845) do
+ActiveRecord::Schema.define(version: 20160609220802) do
 
-  create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "content", collation: "latin1_swedish_ci"
-    t.bigint "user_id"
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "picture", collation: "latin1_swedish_ci"
+    t.string "picture"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
-  create_table "phones", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
     t.datetime "created_at", null: false
@@ -37,21 +31,20 @@ ActiveRecord::Schema.define(version: 2020_11_06_173845) do
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", collation: "latin1_swedish_ci"
-    t.string "name", collation: "latin1_swedish_ci"
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest", collation: "latin1_swedish_ci"
-    t.string "remember_digest", collation: "latin1_swedish_ci"
+    t.string "password_digest"
+    t.string "remember_digest"
     t.boolean "admin", default: false
-    t.string "activation_digest", collation: "latin1_swedish_ci"
+    t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
-    t.string "reset_digest", collation: "latin1_swedish_ci"
+    t.string "reset_digest"
     t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "microposts", "users"
 end
